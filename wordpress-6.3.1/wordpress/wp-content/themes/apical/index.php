667a5860e4ff1baf050a2cee52b623d3e3515fa7
Bicarbonate Bicarbonate <allo>
2023-10-18 22:51:59 -0400

    final

--- wordpress-6.3.1/wordpress/wp-content/themes/apical/index.php
+++ wordpress-6.3.1/wordpress/wp-content/themes/apical/index.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 <!DOCTYPE html>
 <html>
-<head>
-  <title></title>
-</head>
-<body>
-  <?php include get_template_directory() . '/content.php'; ?><?php get_footer(); ?>
-</body>
+   <head>
+      <title></title>
+   </head>
+   <body>
+      <?php include get_template_directory() . '/content.php'; ?><?php get_footer(); ?>
+   </body>
 </html>
